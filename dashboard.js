303117c442c051afdef6c72bc3e5a4e2a384f7a7
thapelomaloka <thaloka3648@gmail.com>
2022-05-28 09:00:11 +0200

    New Update
    Created  a Landing page with with a sign In feature.

---
+++ dashboard.js
@@ -0,0 +1,38 @@
+document.getElementById("mybutton").onclick = function(){
+
+    let username;
+
+    username = document.getElementById("textbox").value
+    let age = document.getElementById("Age").value
+    age = Number(age)
+    let w = "Welcome Back"
+    
+    if( username == "" && age == ""){
+
+        alert("Please Input A Value");
+        return false;
+    } 
+    else if( username == ""){
+        alert("Please Enter Your Name ")
+        return false
+    }
+
+    if(age == ""  ){
+        alert("Please Enter Your Age");
+        return false;
+    }
+    
+    if(age < 18){
+        
+        document.getElementById('DisplayOnScreen').innerHTML = w + " "+username + " You are " + age+" yeasr Old, unfortunately you are young to vote "; 
+    
+    }
+    
+    else{
+            
+        document.getElementById('DisplayOnScreen').innerHTML = w + " "+username + " You are " + age+" yeasr Old"; 
+    
+    }
+
+        
+}
--- index.js
+++ index.js
@@ -2,75 +2,42 @@
 //let username = window.prompt("Whats Your Name?")
 //console.log("Wlecome back " + username)
 
-/* 
+document.getElementById("signin").onclick = function(){
 
-An Algorithm for taking in an ID number:
-1.Enter an ID number.
-2.Take the 1st 2 number and place in var.
-3.Convert the var to a Number(integer).
-4.Create a var to store the current year.
-5.Create a var to calulate the year:
-age = year - Number
-6.Check if the age fall within the voting age.
-7.Display the result to the user.
+    let username1 = document.getElementById("usernameinp").value;
+    let passcode = String(document.getElementById("passcodeinp").value);
 
-
-*/
-var loader = document.getElementById("preLoad");
-window.addEventListener("load",  function(){
-    loader.style.display = "none";
-})
-
-document.getElementById("mybutton").onclick = function(){
-
-    let username;
-
-    username = document.getElementById("textbox").value;
-    let age = String(document.getElementById("idnumber").value);
-    let idnum = age.toString();
-    let bornyear = "";
-    
-    
-        
-    bornyear += idnum.slice(0,2)
-
-     
-    
-    let yourage = 2022 - parseInt(bornyear);
-    let actualAge = (yourage.toString()).substring(2,4);
     
 
+    let user1 = "ThapeloMaloka"
+    let pass1 = "M@loka3648"
 
-
-
-    let w = "Welcome Back"
-    
-    if( username == "" && age == ""){
-
-        alert("Please Input A Value");
-        return false;
-    } 
-    else if( username == ""){
-        alert("Please Enter Your Name ")
-        return false
-    }
-
-    if(age == ""  ){
-        alert("Please Enter Your Age");
+    if(username1 == "" && passcode == ""){
+        alert("Please Enter your Sign In Credentials")
         return false;
     }
-    
-    if(actualAge < 18){
-        
-        document.getElementById('DisplayOnScreen').innerHTML = w + " "+username + " You are " + actualAge+" yeasr old, unfortunately you are too young to vote"; 
-    
+    else if(username1 == ""){
+        alert("Please enter your user ");
+        return false;
     }
-    
+    else if(passcode == ""){
+        alert("Please enter your passcode ");
+        return false;
+    }
     else{
-            
-        document.getElementById('DisplayOnScreen').innerHTML = w + " "+username + " You are " + actualAge+" yeasr Old, and you are elegible to vote."; 
-    
+        if(username1 == user1 && passcode == pass1){
+            console.log("Signing in....")
+            location.replace("/Dashboard.html")
+        }else{
+            alert("Your Username or Passcode is Incorect")
+        }
     }
-        
+
 }
+
+
+
+
+
+
 
